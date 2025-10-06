b08aecbcbfc089de54ade6277401e1c4771998e6
reyzaw304 <57722622+reyzaw304@users.noreply.github.com>
2025-10-06 17:10:03 +0900

    add create short url and save url to mongodb, redirect using short url

--- index.js
+++ index.js
@@ -5,28 +5,30 @@ const app = express();
 const bodyParser = require('body-parser')
 const dns = require('node:dns');
 
+// import models
+const { getNextSequence } = require('./src/services/idService');
+const Url = require('./src/models/url');
+
+// connect to database
+require('./src/database');
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-// create application/json parser
-const jsonParser = bodyParser.json()
 
 // create application/x-www-form-urlencoded parser
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-// var count = 0
-var urls = []
-
-function findURL(urls, original_url) {
-  for (let i = 0; i < urls.length; i++) {
-    if (urls[i] == original_url) {
-      return i
-    }
-  }
-  urls.push(original_url)
-  return urls.length - 1
-}
+// function findURL(urls, original_url) {
+//   for (let i = 0; i < urls.length; i++) {
+//     if (urls[i] == original_url) {
+//       return i
+//     }
+//   }
+//   urls.push(original_url)
+//   return urls.length - 1
+// }
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -39,24 +41,58 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' })
 });
 
-app.post('/api/shorturl', urlencodedParser, (req, res, next) => {
+async function verifyUrl(input) {
   // check if its in url format
   let urlObj
-  try {
-    urlObj = new URL(req.body.url)
-  } catch {
-    console.log("not url format")
-    return res.json({ error: 'invalid url' })
-  }
-  let hostname = urlObj.hostname
+  urlObj = new URL(input)
+  // dns lookup
+  const { lookup } = dns.promises
+  await lookup(urlObj.hostname)
 
-  dns.lookup(hostname, (err, address) => {
-    if (err) {
-      console.log("failed dns lookup")
-      return res.json({ error: 'invalid url' })
+  return urlObj
+}
+
+app.post('/api/shorturl', urlencodedParser, async (req, res, next) => {
+  let urlObj
+  try {
+    urlObj = await verifyUrl(req.body.url)
+    if (urlObj.protocol !== 'http:' && urlObj.protocol !== 'https:') {
+      throw new Error('invalid protocol')
     }
-  })
-  return res.json({ hostname: hostname })
+  } catch {
+    return res.json({ error: 'invalid url' });
+  }
+
+  // check if url already exists
+  let foundUrl = await Url.findOne({ original_url: urlObj.href }).lean()
+  if (foundUrl) {
+    return res.json({ original_url: foundUrl.original_url, short_url: foundUrl.short_url })
+  } else {
+    // get next sequence
+    let nextId = await getNextSequence('short_url')
+    console.log({
+      nextId,
+      type: typeof nextId,
+      url: urlObj.href
+    });
+
+    let newUrl = new Url({ original_url: urlObj.href, short_url: nextId })
+    await newUrl.save()
+    return res.json({ original_url: newUrl.original_url, short_url: newUrl.short_url })
+  }
+})
+
+app.get('/api/shorturl/:short_url', async (req, res, next) => {
+  let id = Number(req.params.short_url)
+  if (isNaN(id)) {
+    return res.status(400).json({ error: 'Wrong format' })
+  }
+
+  const doc = await Url.findOne({ short_url: id }).lean()
+  if (!doc) {
+    return res.status(400).json({ error: 'invalid url' })
+  }
+  return res.redirect(doc.original_url)
 })
 
 app.listen(port, function () {
